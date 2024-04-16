8232cb1b104a48b4cb137e9ecd133b82250e7046
Ilia Mediankin <i.mediankin@gmail.com>
2024-04-16 16:01:51 +0300

    feat: templates(welcome, footer) filter(concat_strings) simple_tag(path_filter)

--- users/templatetags/user_tags.py
+++ users/templatetags/user_tags.py
@@ -1,11 +1,14 @@
 from django import template
-from django.contrib.auth import get_user_model
 
 register = template.Library()
 
 
-# @register.simple_tag
-# def get_user_image():
-#     image = get_user_model().image.value
-#     print(image)
-#     return image
+@register.filter
+def concat_strings(str1, str2):
+    return str1 + str2
+
+
+@register.simple_tag
+def path_filter():
+    filter_url = '/media/playground.com'
+    return filter_url
