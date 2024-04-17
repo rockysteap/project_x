bf8082cd47606cb0e2f706c73627303532c51a6d
Ilia Mediankin <i.mediankin@gmail.com>
2024-04-17 15:59:04 +0300

    feat: add dashboard models(article, image)

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,45 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.text import slugify
+
+from dashboard.management.utils.transliterator import Transliterator
+
+
+class Article(models.Model):
+    class Category(models.TextChoices):
+        PUBLIC = 'PUBLIC'
+        STAFF = 'STAFF'
+
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='articles',
+                               null=True, blank=True)
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=250, unique=True)
+    category = models.CharField(choices=Category.choices, default=Category.STAFF, max_length=6,
+                                verbose_name='Категория')
+    content = models.TextField(blank=True, verbose_name='Текст статьи')
+    time_created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_posted = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    is_published = models.BooleanField(default=False, verbose_name='Статус')
+    image = models.ManyToManyField('Image', blank=True, related_name='articles', verbose_name='Изображения')
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(Transliterator.transliterate_ru_to_en(self.title))
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новости'
+        ordering = ['-time_created']
+        indexes = [
+            models.Index(fields=['-time_created'])
+        ]
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to="articles/images/%Y/%m/%d", blank=True, null=True, verbose_name="Фотография")
+    description = models.CharField(verbose_name='Описание', max_length=250, blank=True, null=True)
 
 
 class Course(models.Model):
