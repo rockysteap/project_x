e239cf96dd00db1807eef3291a31d7eb90100422
Ilia Mediankin <i.mediankin@gmail.com>
2024-04-18 14:13:21 +0300

    feat(photo_handler): revise url path checking and slicing

--- users/templatetags/user_tags.py
+++ users/templatetags/user_tags.py
@@ -3,11 +3,11 @@ from django import template
 register = template.Library()
 
 
-@register.filter()
+@register.filter
 def concat_strings(str1, str2):
     return str1 + str2
 
 
-@register.simple_tag()
+@register.simple_tag
 def external_image_hosting():
     return 'iimg.su'
